2705f4e27e3a73b93ad127c0cdee632c3659f64f
Jonatan <jonguz6@wp.pl>
2021-03-27 13:18:56 +0100

    feature(NG): create model for ingredients as FoodIngredient

--- food_storage/models.py
+++ food_storage/models.py
@@ -1,7 +1,29 @@
 from django.db import models
 
+
 class FoodCategory(models.Model):
-    name = models.Charfield(max_length=20)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class FoodIngredient(models.Model):
+    FOOD_GROUPS = [
+        ("f", "Fruits"),
+        ("v", "Vegetables"),
+        ("g", "Grains"),
+        ("p", "Protein Foods"),
+        ("d", "Dairy"),
+    ]
+    name = models.CharField(max_length=30)
+    category = models.ForeignKey(FoodCategory)
+    food_group = models.CharField(max_length=2, choices=FOOD_GROUPS)
+    carbohydrates = models.FloatField()
+    fats = models.FloatField()
+    protein = models.FloatField()
+    quantity = models.IntegerField(default=1)
+    standard_portion = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
