473011ec02dd2e804407316973a053c945a2656b
Jonatan <jonguz6@wp.pl>
2021-03-27 15:35:12 +0100

    fix(NG): added success urls for views

--- food_storage/views.py
+++ food_storage/views.py
@@ -1,5 +1,6 @@
 import django.views.generic as views
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 from food_storage import models
 
@@ -8,6 +9,7 @@ class FoodCategoryCreateView(views.CreateView):
     model = models.FoodCategory
     fields = "__all__"
     template_name = "FoodCategory/category-create.html"
+    success_url = reverse_lazy('food_storage:category-list')
 
 
 class FoodCategoryListView(views.ListView):
@@ -23,11 +25,13 @@ class FoodCategoryDetailView(views.DetailView):
 class FoodCategoryUpdateView(views.UpdateView):
     model = models.FoodCategory
     template_name = "FoodCategory/category-update.html"
+    success_url = reverse_lazy('food_storage:category-list')
 
 
 class FoodCategoryDeleteView(views.DeleteView):
     model = models.FoodCategory
     template_name = "FoodCategory/category-delete.html"
+    success_url = reverse_lazy('food_storage:category-list')
 
 
 def index(request):
