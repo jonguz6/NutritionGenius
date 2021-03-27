b19a6d6ff3eae77c572d54b82ed71574d9e440d1
Jonatan <jonguz6@wp.pl>
2021-03-27 11:51:19 +0100

    feature(NG): Add signals to create Profile on user creation

--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,21 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     weight = models.FloatField(blank=True)
     height = models.IntegerField(blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
