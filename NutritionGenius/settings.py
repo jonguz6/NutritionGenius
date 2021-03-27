6e696248f2c0d197ea24948fc21440650db31bf6
Jonatan <jonguz6@wp.pl>
2021-03-27 13:37:13 +0100

    fix(NG): register app food_storage in settings

--- NutritionGenius/settings.py
+++ NutritionGenius/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'profiles'
+    'profiles',
+    'food_storage',
 ]
 
 MIDDLEWARE = [
