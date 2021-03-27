161c9e3e18c6e8735e9338446393748fad1253e6
Jonatan <jonguz6@wp.pl>
2021-03-27 11:39:17 +0100

    Revert "create model Profile"
    This reverts commit 2ef0d55249b9a44dbedc50471d7e73c583325c08.

--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,3 @@
-from django.contrib.auth.models import User
 from django.db import models
 
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    weight = models.FloatField(blank=True)
-    height = models.IntegerField(blank=True)
+# Create your models here.
