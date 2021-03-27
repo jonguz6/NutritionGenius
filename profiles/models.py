2ef0d55249b9a44dbedc50471d7e73c583325c08
Jonatan <jonguz6@wp.pl>
2021-03-27 11:14:14 +0100

    create model Profile

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    weight = models.FloatField(blank=True)
+    height = models.IntegerField(blank=True)
