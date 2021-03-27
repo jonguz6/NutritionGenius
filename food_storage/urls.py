17d07fa392c2ad363ca052c47204c190dd958bc8
Jonatan <jonguz6@wp.pl>
2021-03-27 15:22:57 +0100

    create(NG): create and register CRUD views for FoodCategory model

--- food_storage/urls.py
+++ food_storage/urls.py
@@ -4,4 +4,22 @@ from food_storage import views
 
 app_name = "food_storage"
 urlpatterns = [
+    path('', views.index, name="index"),
+    
+    path('category-create/', 
+         views.FoodCategoryCreateView.as_view(), 
+         name="category-create"),
+    path('category-list/',
+         views.FoodCategoryListView.as_view(),
+         name="category-list"),
+    path('category-detail/<pk>/',
+         views.FoodCategoryDetailView.as_view(), 
+         name="category-detail"),
+    path('category-update/<pk>/',
+         views.FoodCategoryUpdateView.as_view(), 
+         name="category-update"),
+    path('category-delete/<pk>/',
+         views.FoodCategoryDeleteView.as_view(), 
+         name="category-delete"),
+    
 ]
--- food_storage/views.py
+++ food_storage/views.py
@@ -1,3 +1,38 @@
+import django.views.generic as views
 from django.shortcuts import render
 
-# Create your views here.
+from food_storage import models
+
+
+class FoodCategoryCreateView(views.CreateView):
+    model = models.FoodCategory
+    fields = "__all__"
+    template_name = "FoodCategory/category-create.html"
+
+
+class FoodCategoryListView(views.ListView):
+    model = models.FoodCategory
+    template_name = "FoodCategory/category-list.html"
+
+
+class FoodCategoryDetailView(views.DetailView):
+    model = models.FoodCategory
+    template_name = "FoodCategory/category-detail.html"
+
+
+class FoodCategoryUpdateView(views.UpdateView):
+    model = models.FoodCategory
+    template_name = "FoodCategory/category-update.html"
+
+
+class FoodCategoryDeleteView(views.DeleteView):
+    model = models.FoodCategory
+    template_name = "FoodCategory/category-delete.html"
+
+
+def index(request):
+    return render(
+        request, template_name="food_storage-index.html"
+    )
+
+
