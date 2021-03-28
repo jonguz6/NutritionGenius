87b79cbfd7578d8c766893f475312f6bda888d23
Jonatan <jonguz6@wp.pl>
2021-03-28 09:30:50 +0200

    change(NG): Refactor html templates

--- templatetags/common_tags.py
+++ templatetags/common_tags.py
@@ -11,3 +11,6 @@ def to_verbose_name(value):
 def to_verbose_plural_name(value):
     return value._meta.verbose_name_plural
 
+@register.filter
+def to_model_verbose_name(value):
+    return value.model._meta.verbose_name
