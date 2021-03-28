b733cbcf0bb6631ca4182b4ca40d63a4c6d0d3c3
Jonatan <jonguz6@wp.pl>
2021-03-28 13:04:59 +0200

    feature(NG): Added Profile inline in User Admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
+from profiles.forms import ProfileForm
 from profiles.models import Profile
 
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    verbose_name_plural = "Profile"
+    can_delete = False
+    form = ProfileForm
+
+
+class MyUserAdmin(UserAdmin):    
+    inlines = [
+        ProfileInline,
+    ]
+
+
 admin.site.register(Profile)
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
