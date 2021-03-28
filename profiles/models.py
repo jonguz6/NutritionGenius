28db8b90129a61fd76f796d4322c24c13aa87d5d
Jonatan <jonguz6@wp.pl>
2021-03-28 14:36:22 +0200

    change(NG): Using UserFoodStorage as through table

--- profiles/models.py
+++ profiles/models.py
@@ -3,7 +3,20 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from food_storage import models as food_models
+from food_storage.models import FoodIngredient
+
+
+class FoodItem(models.Model):
+    ingredient = models.ForeignKey(FoodIngredient,
+                                   related_name="food_items",
+                                   on_delete=models.PROTECT)
+    quantity = models.IntegerField(default=1)
+
+    class Meta:
+        unique_together = (('ingredient', "quantity"),)
+
+    def __str__(self):
+        return f"{self.ingredient.__str__} q: {self.quantity}"
 
 
 class Profile(models.Model):
@@ -13,6 +26,9 @@ class Profile(models.Model):
                                 primary_key=True)
     weight = models.FloatField()
     height = models.IntegerField()
+    daily_food = models.ManyToManyField(FoodItem,
+                                        through="UserFoodStorage",
+                                        related_name="daily_food")
 
     def __str__(self):
         if self.user.first_name != "" or self.user.last_name != "":
@@ -21,18 +37,20 @@ class Profile(models.Model):
 
 
 class UserFoodStorage(models.Model):
-    date = models.DateTimeField()
-    user = models.ForeignKey(User,
+    date = models.DateField(auto_created=True)
+    user = models.ForeignKey(Profile,
                              on_delete=models.CASCADE,
                              related_name="food_storage")
-    food = models.ManyToManyField(food_models.FoodIngredient,
-                                  related_name="food_storage")
+    food = models.ForeignKey(FoodItem,
+                             on_delete=models.CASCADE,
+                             related_name="food_storage",
+                             null=True)
 
     class Meta:
         unique_together = (('date', "user"),)
 
     def __str__(self):
-        return f"Storage of user {self.user.__str__}"
+        return f"Storage of user {self.user.__str__} for {self.date}"
 
 
 @receiver(post_save, sender=User)
