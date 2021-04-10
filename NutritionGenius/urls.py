9e3697f58df7f3af3f08c81760c9320daa9dc73b
Jonatan <jonguz6@wp.pl>
2021-04-10 15:25:10 +0200

    feature(NG): added about page

--- NutritionGenius/urls.py
+++ NutritionGenius/urls.py
@@ -20,6 +20,7 @@ from NutritionGenius import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('about/', views.about, name="about"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('food-storage/', include('food_storage.urls')),
