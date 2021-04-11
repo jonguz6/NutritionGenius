07b4a298561b971fc6a9f05331144a42b7cebe7e
Jonatan <jonguz6@wp.pl>
2021-04-11 10:23:01 +0200

    feature(NG): new project tag

--- templatetags/common_tags.py
+++ templatetags/common_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.template.defaultfilters import floatformat
 
 register = template.Library()
 
@@ -7,10 +8,17 @@ register = template.Library()
 def to_verbose_name(value):
     return value._meta.verbose_name
 
+
 @register.filter
 def to_verbose_plural_name(value):
     return value._meta.verbose_name_plural
 
+
 @register.filter
 def to_model_verbose_name(value):
     return value.model._meta.verbose_name
+
+
+@register.filter(name='abs')
+def abs_filter(value):
+    return abs(value)
