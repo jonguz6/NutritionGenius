c17742a5f8ce120194d1ae0b11e03284bd628b10
Jonatan <jonguz6@wp.pl>
2021-04-11 15:09:17 +0200

    change(NG): add unique constraint on food category

--- food_storage/models.py
+++ food_storage/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class FoodCategory(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
 
     class Meta:
         verbose_name = "Food Category"
