01fd76df78aa82291fa93d8475b5c2d521491d52
Jonatan <jonguz6@wp.pl>
2021-04-19 20:26:14 +0200

    feature(NG): enable postgres on heroku

--- NutritionGenius/settings.py
+++ NutritionGenius/settings.py
@@ -88,12 +88,24 @@ WSGI_APPLICATION = 'NutritionGenius.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'd42t8p7jkj2ij5',
+            'USER': get_env_value('PGUSER'),
+            'PASSWORD': get_env_value('PGPASSWORD'),
+            'HOST': get_env_value('DATABASE_URL'),
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
